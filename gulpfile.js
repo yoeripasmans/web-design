06f21267eeaa8fdb4694557063ded7058fa29223
Yoeri Pasmans <yoerip@live.nl>
2018-04-09 11:31:13 +0200

    Setup project

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'src'
+    },
+});
+});
+
+gulp.task('watch', ['browserSync'], function (){
+  gulp.watch('src/**/*.css', browserSync.reload);
+  gulp.watch('src/**/*.html', browserSync.reload);
+  gulp.watch('src/**/*.js', browserSync.reload);
+});
