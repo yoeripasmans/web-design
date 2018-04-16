fadae2f1f8301e99fd629026738f81bed298c568
Yoeri Pasmans <yoerip@live.nl>
2018-04-16 12:50:31 +0200

    Project setup

---
+++ opdracht2/gulpfile.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp');
+var imagemin = require('gulp-imagemin');
+var browserSync = require('browser-sync').create();
+
+gulp.task('browserSync', function() {
+    browserSync.init({
+        server: {
+            baseDir: 'src'
+        }
+    });
+});
+
+gulp.task('watch', ['browserSync'], function() {
+    gulp.watch('src/**/*.css', browserSync.reload);
+    gulp.watch('src/**/*.html', browserSync.reload);
+    gulp.watch('src/**/*.js', browserSync.reload);
+});
