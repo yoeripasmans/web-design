da8543ecb3fffc762ee85d1ddd81d31945c7f62e
Yoeri Pasmans <yoerip@live.nl>
2018-04-18 19:16:29 +0200

    Added logo's and icon

--- opdracht2/src/assets/js/index.js
+++ opdracht2/src/assets/js/index.js
@@ -7,7 +7,7 @@ var scroll = new SmoothScroll('a[href*="#"]', {
 	speed: 500, // Integer. How fast to complete the scroll in milliseconds
 	offset: 0, // Integer or Function returning an integer. How far to offset the scrolling anchor location in pixels
 	easing: 'easeInOutCubic', // Easing pattern to use
-	customEasing: function (time) {
+	customEasing: function(time) {
 
 		// Function. Custom easing pattern
 		// If this is set to anything other than null, will override the easing option above
@@ -20,10 +20,59 @@ var scroll = new SmoothScroll('a[href*="#"]', {
 	},
 
 	// Callback API
-	before: function (anchor, toggle) {}, // Callback to run before scroll
-	after: function (anchor, toggle) {} // Callback to run after scroll
+	before: function(anchor, toggle) {}, // Callback to run before scroll
+	after: function(anchor, toggle) {} // Callback to run after scroll
 });
 
 window.sr = ScrollReveal();
 var nodeList = document.querySelectorAll('.content');
 sr.reveal(nodeList);
+
+// var navItems = document.querySelectorAll('.main-nav ul li');
+// for (var i = 0; i < navItems.length; i++) {
+// 	if (navItems[i].classList.contain('active')) {
+// 		navItems[i].addEventListener('click', toggleActive);
+// 	}
+// }
+//
+//
+// function toggleActive(e) {
+// 	console.log(navItems[i]);
+// 	// if (this.classList.contain('active')) {
+// 		this.classList.add('active');
+// 	// } else {
+// 	//
+// 	// }
+// }
+
+window.onscroll = onScroll;
+
+function onScroll() {
+    var removeActiveClass = function (elements) {
+        for (var i = 0; i < elements.length; ++i) {
+            elements[i].classList.remove('active');
+        }
+    };
+    var anchors = document.querySelectorAll('.main-nav a');
+    var previousRefElement = null;
+    for (var i = 0; i < anchors.length; ++i) {
+        // Get the current element by the id from the anchor's href.
+        var currentRefElement = document.getElementById(anchors[i].getAttribute('href').substring(1));
+        var currentRefElementTop = (currentRefElement.getBoundingClientRect().top - 68);
+        // Searching for the element whose top haven't left the top of the browser.
+        if (currentRefElementTop <= 0) {
+            //The browser's top line haven't reached the current element, so the previous element is the one we currently look at.
+            previousRefElement = anchors[i];
+            // Edge case for last element.
+            if (i == anchors.length - 1) {
+                removeActiveClass(anchors);
+                anchors[i].classList.add("active");
+            }
+        } else {
+            removeActiveClass(anchors);
+            previousRefElement.classList.add("active");
+            break;
+        }
+
+    }
+}
